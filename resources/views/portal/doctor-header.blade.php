b0f5397b2c862aca44fd825c642dc8b506e65af6
RithiBaskaran <baskar2271@yahoo.com>
2017-02-17 11:38:02 +0530

    Prescription App

--- resources/views/portal/doctor-header.blade.php
+++ resources/views/portal/doctor-header.blade.php
@@ -5,7 +5,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><img src="{{ URL::to('/') }}/images/head-logoicon.png" width="100%"></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><img src="{{ URL::to('/') }}/images/head-logosmall.png" width="100%"></span>
+        <span class="logo-lg"><img src="{{ URL::to('/') }}/images/head-logo-new.png" ></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
